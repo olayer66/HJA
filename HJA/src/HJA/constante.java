4417b320ec985571b7021d58d5cf342fa69b0397
aa <aa@aa-PC>
2015-11-12 17:17:49 +0100

    constante modificado sin terminar

--- HJA/src/HJA/constante.java
+++ HJA/src/HJA/constante.java
@@ -1,5 +1,34 @@
 package HJA;
 
-public class constante {
+import java.util.HashMap;
 
+public class constante 
+{
+		   private static constante instance = null;
+		   private HashMap<String,datosMano> manos;
+		   public class datosMano
+		   {
+			   private int x;
+			   private int y;
+			   private int valorChubukov;
+			   private int valor2;
+			   public datosMano(int miX,int miY, int valorCH,int v2)
+			   {
+				   x=miX;
+				   y=miY;
+				   valorChubukov=valorCH;
+				   valor2=v2;
+			   }
+			   
+		   }
+		   protected constante() {
+		      // Exists only to defeat instantiation.
+		   }
+		   public static constante getInstance() {
+		      if(instance == null) 
+		      {
+		         instance = new constante();
+		      }
+		      return instance;
+		   }
 }
